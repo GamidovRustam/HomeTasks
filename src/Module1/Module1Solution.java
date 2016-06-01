edfe1b0e34b68f1f687a3ad9c9129099de15e619
GamidovRustam <rustformail@gmail.com>
2016-06-01 14:01:53 +0300

    pushed from IDEA

---
+++ src/Module1/Module1Solution.java
@@ -0,0 +1,11 @@
+package Module1;
+
+/**
+ * Created by Rustam Gamidov on 19.05.2016.
+ */
+public class Module1Solution {
+    public static void main(String[] args) {
+        String s = "Hi, I'm your first Java program! Stay tuned!";
+        System.out.println(s);
+    }
+}
