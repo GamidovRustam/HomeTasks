813a1e7b434e027d17766fe717336bf115c0123a
GamidovRustam <rustformail@gmail.com>
2016-06-04 17:52:28 +0300

    Final version

--- src/Module04/Task1/CounterOfShapesArea.java
+++ src/Module04/Task1/CounterOfShapesArea.java
@@ -4,9 +4,11 @@ public class CounterOfShapesArea {
     public double countTriangleArea(double base, double hight){
         return (base * hight)*0.5;
     }
+
     public double countRectangleArea(double sideA, double sideB){
         return sideA * sideB;
     }
+
     public double countCircleArea(double radius){
         return (Math.ceil(Math.PI * Math.pow(radius,2)*100))/100;
     }
--- src/Module04/Task2/TemperatureConverter.java
+++ src/Module04/Task2/TemperatureConverter.java
@@ -4,6 +4,7 @@ public class TemperatureConverter {
     public double convertCtoF(double celsius){
         return Math.ceil((celsius*(9.0/5.0)+32)*100)/100;
     }
+
     public double convertFtoC(double fahrenheit){
         return Math.ceil(((fahrenheit-32)*(5.0/9.0))*100)/100;
     }
