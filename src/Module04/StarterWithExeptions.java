4e21b1c5e9dbc8663abcedc8f11e51a8bb728fcb
GamidovRustam <rustformail@gmail.com>
2016-06-13 01:45:13 +0300

    Added an exeption

--- src/Module04/StarterWithExeptions.java
+++ src/Module04/StarterWithExeptions.java
@@ -4,85 +4,192 @@ import Module04.Task1.CounterOfShapesArea;
 import Module04.Task2.TemperatureConverter;
 import Module04.Task3.DistanceCalculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class StarterWithExeptions {
+    private static String error = "ERROR: Please,restart program and enter the number >0 in format # or #,#:";
+
     public static void main(String[] args) {
-        String error = "not correct! Please,restart program and enter the number in format # or #,#:";
 
         Scanner sc = new Scanner(System.in);
         CounterOfShapesArea countArea = new CounterOfShapesArea();
-        System.out.println("Let's calculate some shapes area, and we begin with triangle.");
-        System.out.println("Enter the hight of triangle:");
-
-//        final double userInput = sc.nextDouble();
+        System.out.println("Let's calculate some shapes area, and we begin with triangle." + "\n"
+                            + "Enter the hight of triangle:");
 
         try {
-//            final double age = Double.parseDouble(userInput);
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+                countArea.setTriangleHight(userInput);
+            }catch(InputMismatchException ex){
+                System.out.println(error);
+                return;
+            }
 
-
-//            System.out.println(age);
-            countArea.setTriangleHight(sc.nextDouble());
-        } catch (NumberFormatException ex){
-            System.out.println("nonono ");
-        }
             System.out.println("now enter the lenght of the base:");
-            countArea.setTriangleBase(sc.nextDouble());
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            countArea.setTriangleBase(userInput);
+            } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+            }
+
             System.out.println("Triangle area = " +
                     countArea.countTriangleArea(countArea.getTriangleHight(), countArea.getTriangleBase())
-                    + " square meters");
-            System.out.println("");
+                    + " square meters" + "\n");
 
+            System.out.println("Well done! Now let's calculate the rectangle area." + "\n" + "enter sideA lenght:");
 
-            System.out.println("Well done! Now let's calculate the rectangle area.");
-            System.out.println("enter sideA lenght:");
-            countArea.setRectSideA(sc.nextDouble());
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            countArea.setRectSideA(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
             System.out.println("and sideB:");
-            countArea.setRectSideB(sc.nextDouble());
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            countArea.setRectSideB(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
             System.out.println("Rectangle area = " +
                     countArea.countRectangleArea(countArea.getRectSideA(), countArea.getRectSideB()) +
-                    " square meters");
-            System.out.println("");
-            System.out.println("And the last shape is a circle:");
-            System.out.println("enter the radius of a circle:");
+                    " square meters" + "\n");
+            System.out.println("And the last shape is a circle:" + "\n" + "enter the radius of a circle:");
 
-            countArea.setRadius(sc.nextDouble());
-            System.out.println("Circle area = " + countArea.countCircleArea(countArea.getRadius()) + " square meters");
-            System.out.println("");
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            countArea.setRadius(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
 
+            System.out.println("Circle area = " + countArea.countCircleArea(countArea.getRadius())
+                                + " square meters" + "\n");
 
             TemperatureConverter temperature = new TemperatureConverter();
-            System.out.println("Now, you can convert the temperature grades.");
-            System.out.println("enter the Celsius grades:");
+            System.out.println("Now, you can convert the temperature grades." + "\n" + "enter the Celsius grades:");
 
-            temperature.setCelsius(sc.nextDouble());
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            temperature.setCelsius(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
             System.out.println("in Fahrenheit it will be " + temperature.convertCtoF(temperature.getCelsius()));
             System.out.println("and Fahrenheit to Celsius (enter grades):");
 
-            temperature.setFahrenheit(sc.nextDouble());
-            System.out.println("in Celsius it will be " + temperature.convertFtoC(temperature.getFahrenheit()));
-            System.out.println("");
-
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            temperature.setFahrenheit(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+            System.out.println("in Celsius it will be " + temperature.convertFtoC(temperature.getFahrenheit()) + "\n");
 
             DistanceCalculator distance = new DistanceCalculator();
-            System.out.println("Ok! Now you can calculate the distance between 2 points");
-            System.out.println("Enter point A coordinate X");
-            //очень много одинаковых кусков кода. Подумай, как это можно оптимизировать
-            distance.setPointAX(sc.nextDouble());
-            System.out.println("Enter point A coordinate Y");
-            distance.setPointAY(sc.nextDouble());
-            System.out.println("Enter point B coordinate X");
-            distance.setPointBX(sc.nextDouble());
-            System.out.println("Enter point B coordinate Y");
-            distance.setPointBY(sc.nextDouble());
+            System.out.println("Ok! Now you can calculate the distance between 2 points" + "\n"
+                                + "Enter point A coordinate X");
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            distance.setPointAX(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
+        System.out.println("Enter point A coordinate Y");
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            distance.setPointAY(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
+        System.out.println("Enter point B coordinate X");
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            distance.setPointBX(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
+        System.out.println("Enter point B coordinate Y");
+
+        try {
+            final double userInput = sc.nextDouble();
+            if (userInput <= 0) {
+                System.out.println(error);
+                return;
+            }
+            distance.setPointBY(userInput);
+        } catch (InputMismatchException ex){
+            System.out.println(error);
+            return;
+        }
+
             System.out.println("The distance between point's A and B = "
                     + distance.countDistance(distance.getPointAX(),
                     distance.getPointAY(),
                     distance.getPointBX(),
-                    distance.getPointBY()) +
-                    " meters.");
+                    distance.getPointBY()) + " meters.");
 
-        System.out.println("That's all! Bye!");
-
+            System.out.println("That's all! Bye!");
     }
 }
