b96a4fc3ffbf626255036b4544bf335a3cc36dbe
GamidovRustam <rustformail@gmail.com>
2016-06-15 19:26:47 +0300

    Draft version. Need no fix (2)

--- src/module06/task1/chekInput/CheckInput.java
+++ src/module06/task1/chekInput/CheckInput.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CheckInput extends Exception{
+
     public static double ifBiggerThanZeroDouble() {
         double userInput = 0;
         boolean lessThanZero = true;
@@ -34,6 +35,22 @@ public class CheckInput extends Exception{
         return userInput;
             }
 
+    public static int ifNegativeValueInt() {
+        int userInput = 0;
+        boolean lessThanZero = true;
+
+        while (lessThanZero) {
+                userInput = ifInt();
+            if (userInput < 0) {
+                System.out.println("Wrong! Please, enter the number bigger than zero:");
+            } else {
+                lessThanZero = false;
+            }
+        }
+        return userInput;
+    }
+
+
     public static double ifDouble() throws InputMismatchException{
         Scanner sc = new Scanner(System.in);
         return sc.nextDouble();
@@ -41,8 +58,18 @@ public class CheckInput extends Exception{
 
     public static int ifInt() throws InputMismatchException{
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        int i;
+        try {
+            i = sc.nextInt();
+        }catch (InputMismatchException e) {
+            System.out.println("Wrong! Please, enter the number:");
+            i = ifInt();
+        }
+        return i;
     }
+
     }
+
+
 
 
--- src/module06/task2/Runner3.java
+++ src/module06/task2/Runner3.java
@@ -1,13 +1,16 @@
 package module06.task2;
 
+import module06.task1.chekInput.CheckInput;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Runner3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Module03.Task3.NotEnoughInstruments {
         MusicShop shop = new MusicShop();
+        CheckInput checkInput = new CheckInput();
 
         Map<String, Integer> asortiment = new HashMap<>();
         asortiment.put(MusicalInstrument.PIANO, 3);
@@ -16,31 +19,51 @@ public class Runner3 {
 
         shop.setMusicalInstruments(asortiment);
 
-        Map<String, Integer> order = new HashMap<>();
-        order.put(MusicalInstrument.PIANO, 3);
-        order.put(MusicalInstrument. GUITAR, 3);
-        order.put(MusicalInstrument.TRUMPET, 2);
+        System.out.println("Make an order:");
 
-        List<MusicalInstrument> list;
+        Map<String, Integer> order = new HashMap<>();
+
+        int orderPiano = checkInput.ifNegativeValueInt();
+        order.put(MusicalInstrument.PIANO, orderPiano);
+
+//        try {
+//            if (order.get(MusicalInstrument.PIANO) >= orderPiano) {
+//                order.put(MusicalInstrument.PIANO, orderPiano);
+//            }
+//        }catch (NotEnoughInstruments e){
+//            System.out.println("We cant complete the order. There is " + e.getMessage());
+//        }
+
+        int orderGuitar = checkInput.ifNegativeValueInt();
+            order.put(MusicalInstrument.GUITAR, orderGuitar);
+
+        int orderTrumpet = checkInput.ifNegativeValueInt();
+        order.put(MusicalInstrument.TRUMPET, orderTrumpet);
+
         try {
-            list = shop.prepareInstruments(order);
-            for (MusicalInstrument instrument : list) {
-                System.out.println(instrument);
-            }
+            shop.prepareInstruments(order);
+            System.out.println("Your order: ");
+            System.out.println("Piano - " + order.get(MusicalInstrument.PIANO));
+            System.out.println("Guitars - " + order.get(MusicalInstrument.GUITAR));
+            System.out.println("Trumpets - " + order.get(MusicalInstrument.TRUMPET));
         } catch (NotEnoughInstruments e) {
             System.out.println("We cant complete the order. There is " + e.getMessage());
         }
-
     }
+}
 
+
+//        List<MusicalInstrument> list;
 //        try {
-//            shop.prepareInstruments(order);
-//            System.out.println("Your order: ");
-//            System.out.println("Piano - " + order.get(MusicalInstrument.PIANO));
-//            System.out.println("Guitars - " + order.get(MusicalInstrument.GUITAR));
-//            System.out.println("Trumpets - " + order.get(MusicalInstrument.TRUMPET));
+//            list = shop.prepareInstruments(order);
+//            for (MusicalInstrument instrument : list) {
+//                System.out.println(instrument);
+//            }
 //        } catch (NotEnoughInstruments e) {
 //            System.out.println("We cant complete the order. There is " + e.getMessage());
 //        }
-    }
+
+
+
+
 //}
